8095af6af4317922d5517235c0d522bec372d72a
deepanshu-g <deepanshu.ghai51@gmail.com>
2018-12-23 15:59:18 +0530

    same image title bug fix

--- ImageHoster/src/main/java/ImageHoster/repository/ImageRepository.java
+++ ImageHoster/src/main/java/ImageHoster/repository/ImageRepository.java
@@ -50,10 +50,12 @@ public class ImageRepository {
     //Executes JPQL query to fetch the image from the database with corresponding title
     //Returns the image in case the image is found in the database
     //Returns null if no image is found in the database
-    public Image getImageByTitle(String title) {
+    public Image getImageByTitle(String title,Integer id) {
         EntityManager em = emf.createEntityManager();
         try {
-            TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where i.title =:title", Image.class).setParameter("title", title);
+            TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where i.title =:title AND i.id =:id", Image.class);
+            typedQuery.setParameter("title", title);
+            typedQuery.setParameter("id", id);
             return typedQuery.getSingleResult();
         } catch (NoResultException nre) {
             return null;
